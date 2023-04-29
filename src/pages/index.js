f5bec83636ba5a980573406c9009955c182abb9c
hiroro-d <d.hiroro.s.business@gmail.com>
2023-04-29 15:37:52 +0900

    📝

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
     <>
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Study</title>
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       </Head>
     </div>
